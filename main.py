775030a8b0e949ec607edc57dcf3d46dc2d02752
huy.do1 <Quochuydo@1994>
2021-12-31 21:59:28 +0700

    Add comment and add function_sum

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def add_function_sum(a, b):
+    return a + b
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -14,3 +16,7 @@ if __name__ == '__main__':
     print_hi('PyCharm test change')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""
+Add some comment here for test changing
+"""
